9b61d37f3ad20a92ada1ae1a876c168303552594
Your Name <you@example.com>
2017-07-13 10:10:37 -0400

    initial commit

---
+++ polymorphismAndInheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismAndInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inheritance means classes inherit properties or methods from a parent class.
+            //e.g.
+            //Trasnportation(Base Class)            
+            //-capacity 
+            //-go 
+            //-stop 
+            //-reverse    
+            //-turn
+        }
+    }
+}
